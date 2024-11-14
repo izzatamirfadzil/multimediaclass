316787a9a35e2a77de0a56fa4ea2911cba56f22f
Najihah 'Aqilah <1211103187@student.mmu.edu.my>
2024-11-14 12:25:10 +0800

    3.Add a greeting text

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-
 // My first javascript code
 //document.getElementById("count-el").innerText = 115;
 
 let count = 0;
 let countEl = document.getElementById("count-el"); // get element by id
+
+// Display greeting message on page load
+let greetingEl = document.getElementById("greeting");
+greetingEl.innerText = "Good day student"; // Display the greeting
 
 function increment() {
     count = count + 1;
@@ -20,3 +23,4 @@ function save() {
     count = 0; // reset count to 0 after save
     countEl.innerText = count;
 }
+
